77979c8145bf5842d0519885c7174299e63d4413
J2TeaM <junookyo@gmail.com>
2015-10-07 03:00:23 +0700

    Add built-in modules

--- plugin.media-type.plugin-name/addon.py
+++ plugin.media-type.plugin-name/addon.py
@@ -1,3 +1,12 @@
 # This will contain the main Python code for your add-on.
 # You can name it whatever you want, since you'll define this
 # Python file in addon.xml as your main script file.
+#
+# ---------------------------------------
+# Built-in modules, uncomment if you need.
+#
+# import xbmc
+# import xbmcgui
+# import xbmcplugin
+# import xbmcaddon
+# import xbmcvfs
